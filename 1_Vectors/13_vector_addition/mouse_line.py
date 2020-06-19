b4b8db6fdda3bab54290ab5c8d1fb0e5f9ef1d28
Bill Ma <mazengou@gmail.com>
2020-06-19 14:47:41 +0800

    Mouse tracing line

---
+++ 1_Vectors/13_vector_addition/mouse_line.py
@@ -0,0 +1,37 @@
+import numpy as np
+from tkinter import *
+import time
+
+WIDTH = 600
+HEIGHT = 400
+
+# def motion(event):
+#     x, y = event.x, event.y
+#     print('{}, {}'.format(x, y))
+
+class Motion(object):
+    """docstring for Motion."""
+
+    def __init__(self, pos):
+        super(Motion, self).__init__()
+        self.pos = pos
+
+    def movment(self, event):
+        self.pos[0] = event.x
+        self.pos[1] = event.y
+
+
+root = Tk()
+canvas = Canvas(root, width=WIDTH, height=HEIGHT)
+root.title("Lines")
+canvas.pack()
+line_pos = Motion(np.zeros(2))
+origin = np.array([WIDTH/2., HEIGHT/2.])
+root.bind('<Motion>', line_pos.movment)
+while True:
+    canvas.create_line(origin[0], origin[1], line_pos.pos[0], line_pos.pos[1])
+    root.update()
+    canvas.delete("all")
+    time.sleep(0.01)
+
+root.mainloop()
